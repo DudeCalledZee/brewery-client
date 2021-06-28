418586f7f413ebe81902b0844c30900858ca28c5
DudeCalledZee <ineedtothink@gmail.com>
2021-06-28 13:29:50 +0100

    adds beerdto and CustomerDto

--- src/main/java/com/bms/breweryclient/BreweryClientApplication.java
+++ src/main/java/com/bms/breweryclient/BreweryClientApplication.java
@@ -1,4 +1,4 @@
-package com.BMS.breweryclient;
+package com.bms.breweryclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/bms/breweryclient/BreweryClientApplicationTests.java
+++ src/test/java/com/bms/breweryclient/BreweryClientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.BMS.breweryclient;
+package com.bms.breweryclient;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
